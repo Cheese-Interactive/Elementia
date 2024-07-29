51254cfa9960a9713d25a354291d58580cd96327
kidkidkidkidkid1 <mozambaniac@gmail.com>
2024-07-29 01:29:21 -0700

    Update/add simple motion effects

--- Assets/Scripts/Util/SimpleRot.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class SimpleRot : MonoBehaviour {
-    [SerializeField] private float rotAmount;
-    void Update() {
-        transform.Rotate(0, 0, rotAmount);
-    }
-}
